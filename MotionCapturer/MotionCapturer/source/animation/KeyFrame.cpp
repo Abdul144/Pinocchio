4bb146020a70c36e533f413cfeaba0198984cfdf
JongHeon Kim <jhoney510@gmail.com>
2012-09-11 22:52:18 +0900

    key frame 클래스 추가

--- MotionCapturer/MotionCapturer/source/animation/KeyFrame.cpp
+++ MotionCapturer/MotionCapturer/source/animation/KeyFrame.cpp
@@ -1,4 +1,25 @@
 
 #include "stdafx.h"
-#include "Animation.h"
+#include "KeyFrame.h"
 
+#include "BoneData.h"
+
+
+/// 본정보 추가
+BoneData* KeyFrame::addBoneData()
+{
+    BoneData *boneData = new BoneData;
+    boneDatas.push_back(boneData);
+
+    return boneData;
+}
+
+BoneData* KeyFrame::getBoneData(int index)
+{
+    return boneDatas[index];
+}
+
+void KeyFrame::setBoneData(int index, BoneData *data)
+{
+    boneDatas[index] = data;
+}
--- MotionCapturer/MotionCapturer/source/animation/KeyFrame.h
+++ MotionCapturer/MotionCapturer/source/animation/KeyFrame.h
@@ -1,1 +1,44 @@
 #pragma once
+
+#include <vector>
+
+#include "../util/defines.h"
+
+using namespace std;
+
+
+class BoneData;
+
+class KeyFrame
+{
+	// 멤버 변수
+private:
+    int startFrame;
+    int duration;					///< 재생할 길이
+
+    vector<BoneData*> boneDatas;		///< 각 본의 Data
+
+
+
+	// 멤버 함수
+public:
+    /// 생성자
+    KeyFrame()
+    {
+    }
+
+
+    /// 본정보 추가
+    BoneData* addBoneData();
+
+    BoneData* getBoneData(int index);
+
+    void setBoneData(int index, BoneData *data);
+
+        
+    // 접근
+    GETTER(int, Duration, duration)
+    GETTER(int, BoneDataCount, boneDatas.size())
+    GETTER(int, StartFrame, startFrame)
+
+}
