9aeb93d17ce9a8e222ca364384e1c04fa02dbadd
JongHeon Kim <jhoney510@gmail.com>
2012-09-11 23:33:04 +0900

    Animation class 추가

--- MotionCapturer/MotionCapturer/source/animation/Animation.cpp
+++ MotionCapturer/MotionCapturer/source/animation/Animation.cpp
@@ -2,3 +2,25 @@
 #include "stdafx.h"
 #include "Animation.h"
 
+#include "KeyFrame.h"
+
+
+///  생성자
+Animation::Animation() : totalFrameCount(0)
+{
+}
+
+
+/// 키프레임 추가
+KeyFrame* Animation::addKeyFrame()
+{
+    KeyFrame *keyFrame = new KeyFrame;
+    keyFrames.push_back(keyFrame);
+
+    return keyFrame;
+}
+
+KeyFrame* Animation::getKeyFrame(int index)
+{
+    return keyFrames[index];
+}
--- MotionCapturer/MotionCapturer/source/animation/Animation.h
+++ MotionCapturer/MotionCapturer/source/animation/Animation.h
@@ -1,1 +1,39 @@
 #pragma once
+
+
+#include <string>
+#include <vector>
+
+#include "../util/defines.h"
+
+using namespace std;
+
+
+class KeyFrame;
+
+class Animation
+{
+	// 멤버 변수
+private:
+    string name;
+    vector<KeyFrame*> keyFrames;		///< 키프레임 배열
+    int totalFrameCount;
+
+
+	// 멤버 변수
+public:
+    ///  생성자
+    Animation();
+
+
+    /// 키프레임 추가
+    KeyFrame* addKeyFrame();
+
+    KeyFrame* getKeyFrame(int index);
+
+
+    // 접근
+    GETTER_CONST_REF(string, Name, name)
+    GETTER(int, KeyFrameCount, keyFrames.size())
+    GETTER(int, TotalFrameCount, totalFrameCount)
+}
